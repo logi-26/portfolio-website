46623379064c4c1e969363e199ebdd88b597baa5
logi-26 <louis_gilmartin@hotmail.co.uk>
2023-07-31 13:21:12 +0100

    refactor: rename functions

--- app.py
+++ app.py
@@ -2,21 +2,20 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
     return render_template("home.html")
   
 @app.route("/cv")
-def var():
+def cv():
     return render_template("cv.html")
   
 @app.route("/work")
-def ifelse():
+def work():
     return render_template("work.html")
   
 @app.route("/personal")
-def for_loop():
+def personal():
     return render_template("personal.html")
   
 if __name__ == "__main__":
